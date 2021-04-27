8d5c54a51e8ba5d366d35ea1b57729462e3f0544
Nathan <nathanraysheryak@gmail.com>
2021-04-27 07:29:26 -0400

    create light/dark themes

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,23 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
 
+import colors from 'vuetify/lib/util/colors'
+
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: colors.purple,
+        secondary: colors.blue,
+        accent: colors.shades.black,
+      },
+      dark: {
+        primary: colors.blue.lighten3,
+        secondary: colors.pink,
+        accent: colors.shades.yellow,
+      },
+    },
+  },
 });
